fb6732f35c27863fe0171755a82112e025c77edc
Michael Schulte <misc@imu.unibe.ch>
2020-10-16 09:25:39 +0200

    update friday

---
+++ _sessions/PlottingIII/eval_submissionsHS2020.R
@@ -0,0 +1,65 @@
+# Competition Analysis
+# MSM
+# Oct 2020
+
+library(googlesheets4)
+library(tidyverse)
+library(patchwork)
+
+raw <- read_sheet(ss = '1qODIQcqM8VrWeEyGl2bCfqFqRE9MBtkJyMXiksSzYVU')
+
+graph1 <- 
+  raw %>%
+  select(2:4) %>%
+  summarise(across(where(is.numeric), ~mean(.x, na.rm = TRUE))) %>%
+  mutate(graph = 'Covid-Boot-Camp')
+
+graph2 <- 
+  raw %>%
+  select(5:7) %>%
+  summarise(across(where(is.numeric), ~mean(.x, na.rm = TRUE))) %>%
+  mutate(graph = 'Arr-PiRaten')
+
+graph3 <- 
+  raw %>%
+  select(8:10) %>%
+  summarise(across(where(is.numeric), ~mean(.x, na.rm = TRUE))) %>%
+  mutate(graph = 'We-R-lost')
+
+graph4 <- 
+  raw %>%
+  select(11:13) %>%
+  summarise(across(where(is.numeric), ~mean(.x, na.rm = TRUE))) %>%
+  mutate(graph = 'R-Pandemi')
+
+result <- 
+bind_rows(graph1, graph2, graph3, graph4) %>%
+  rename('Ästhetik' = 'Ist die Grafik ästhetisch ansprechend?', 
+         'Überzeugend' = 'Überzeugen dich die dargestellten Datenmuster (unabhängig von der Fragestellung)?', 
+         'Relevant' = 'Findest du die dargestellten Datenmuster entscheidungsrelevant in Bezug auf die Fragestellung?') %>%
+  pivot_longer(cols = 1:3, names_to = 'Frage')
+
+single <- ggplot(result, aes(Frage, value)) +
+ geom_point(stat = 'identity') + 
+ facet_wrap(~graph, nrow = 1) +
+  ylim(1,5) +
+  labs(y = 'Bewertung',
+       x = 'Bereich',
+       caption = 'RBootcamp Bern 2020',
+       title = 'Ergebnisse Plotting Competition') +
+  theme_bw()
+
+overall_result <- 
+  result %>%
+    group_by(graph) %>%
+    summarise(overall = mean(value))
+  
+overall_graph <- ggplot(overall_result, aes(graph, overall)) +
+  geom_point() +
+  labs(y = 'Bewertung Across',
+       x = 'Gruppe',
+       caption = 'RBootcamp Bern 2020',
+       title = 'Ergebnisse Plotting Competition') +
+  theme_bw()
+
+overall_graph / single
