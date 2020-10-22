5c0a8c2d57060ea786c51820718202d28f7c8e5e
Michael Schulte <misc@imu.unibe.ch>
2020-10-22 22:26:54 +0200

    thursday

--- _sessions/PlottingIII/eval_submissionsHS2020.R
+++ _sessions/PlottingIII/eval_submissionsHS2020.R
@@ -2,12 +2,13 @@
 # MSM
 # Oct 2020
 
+# load packages
 library(googlesheets4)
 library(tidyverse)
 library(patchwork)
-
+# read in data 
 raw <- read_sheet(ss = '1qODIQcqM8VrWeEyGl2bCfqFqRE9MBtkJyMXiksSzYVU')
-
+# generate graphs for each group  
 graph1 <- 
   raw %>%
   select(2:4) %>%
@@ -31,14 +32,14 @@ graph4 <-
   select(11:13) %>%
   summarise(across(where(is.numeric), ~mean(.x, na.rm = TRUE))) %>%
   mutate(graph = 'R-Pandemi')
-
+# bind together, make long
 result <- 
 bind_rows(graph1, graph2, graph3, graph4) %>%
   rename('Ästhetik' = 'Ist die Grafik ästhetisch ansprechend?', 
          'Überzeugend' = 'Überzeugen dich die dargestellten Datenmuster (unabhängig von der Fragestellung)?', 
          'Relevant' = 'Findest du die dargestellten Datenmuster entscheidungsrelevant in Bezug auf die Fragestellung?') %>%
   pivot_longer(cols = 1:3, names_to = 'Frage')
-
+# plot single result 
 single <- ggplot(result, aes(Frage, value)) +
  geom_point(stat = 'identity') + 
  facet_wrap(~graph, nrow = 1) +
@@ -48,18 +49,18 @@ single <- ggplot(result, aes(Frage, value)) +
        caption = 'RBootcamp Bern 2020',
        title = 'Ergebnisse Plotting Competition') +
   theme_bw()
-
+# plot overall
 overall_result <- 
   result %>%
     group_by(graph) %>%
     summarise(overall = mean(value))
   
-overall_graph <- ggplot(overall_result, aes(graph, overall)) +
+overall_graph <- ggplot(overall_result, aes(reorder(graph, -overall), overall)) +
   geom_point() +
   labs(y = 'Bewertung Across',
        x = 'Gruppe',
        caption = 'RBootcamp Bern 2020',
        title = 'Ergebnisse Plotting Competition') +
   theme_bw()
-
+# show next to each other
 overall_graph / single
