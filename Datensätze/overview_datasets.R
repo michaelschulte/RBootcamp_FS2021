89627d4b71f9289c26fbdb15a13da517d5a87a0a
Michael Schulte <misc@imu.unibe.ch>
2021-05-07 09:10:30 +0200

    W2

---
+++ Datensätze/overview_datasets.R
@@ -0,0 +1,29 @@
+#avocado_prices
+df_avocado_prices = read.csv("avocado_prices.csv")
+
+# bike_shares
+df_london_bike_shares = read.csv("london_bike_shares.csv")
+
+# grades
+df_grades_math =read.table("grades_math.csv",sep=";",header=TRUE)
+df_grades_portugese =read.table("grades_portugese.csv",sep=";",header=TRUE)
+
+# diabetes
+df_diabetes = read.csv("diabetes.csv")
+
+# obesity
+df_obesity = read.csv("obesity.csv")
+
+# forest_fires
+df_forest_fires = read.csv("forest_fires.csv")
+
+#df_used_cars = read.csv("used_cars.csv")
+df_used_cars = read.csv("used_cars.csv")
+
+
+
+
+
+
+
+
